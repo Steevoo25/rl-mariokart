d00b96d64644ea4e5460cd31c699297be1a4844f
Harry Stevenson <hjs115@student.bham.ac.uk>
2023-11-03 18:28:06 +0000

    Changed ocr test slightly, cont. writep for prop

--- RL/OCR Test/OCRTest.py
+++ RL/OCR Test/OCRTest.py
@@ -1,11 +1,18 @@
+# A short script to test the functionality of text recognition in images
 from PIL import Image
 import pytesseract
 
-# Open the image file
-image = Image.open('./MkWGeckoCodes.jpg')
+image1 = Image.open('./geckoCropped1.png')
+image2 = Image.open('./geckoCropped2.png')
 
-# Use pytesseract to do OCR on the image
-text = pytesseract.image_to_string(image)
+whitelist_characters = '0123456789.'
+
+custom_config = f'tessedit_char_whitelist={whitelist_characters}'
+
+
+text = pytesseract.image_to_string(image1, config=custom_config)
+text2 = pytesseract.image_to_string(image2, config=custom_config)
 
 # Print the extracted text
-print(text)
+print(f'Image 1:{text}')
+print(f'Image 2:{text2}')
