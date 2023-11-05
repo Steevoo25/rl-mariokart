0704b44c68ecf86161dfeb4d632f2f4f9e6ef753
Harry Stevenson <hjs115@student.bham.ac.uk>
2023-11-05 16:42:33 +0000

    Made minor changes to OCR script and tested new images

--- RL/OCR Test/OCRTest.py
+++ RL/OCR Test/OCRTest.py
@@ -2,17 +2,15 @@
 from PIL import Image
 import pytesseract
 
-image1 = Image.open('./geckoCropped1.png')
-image2 = Image.open('./geckoCropped2.png')
-
+#imagePath = './MkWGeckoCodes.png'
+imagePath = './geckoCropped2.png'
+psm_level = 8
 whitelist_characters = '0123456789.'
+custom_config = f'--psm {psm_level} preserve_interword_spaces=1 textord_dotmatrix_gap=6 classify_bln_numeric_mode=1 rej_alphas_in_number_perm=1'# tessedit_char_whitelist={whitelist_characters}'
 
-custom_config = f'tessedit_char_whitelist={whitelist_characters}'
-
-
+image1 = Image.open(imagePath)
 text = pytesseract.image_to_string(image1, config=custom_config)
-text2 = pytesseract.image_to_string(image2, config=custom_config)
 
-# Print the extracted text
-print(f'Image 1:{text}')
-print(f'Image 2:{text2}')
+image1.close()
+
+print(f'Image 1: {text}with config: {custom_config}')
