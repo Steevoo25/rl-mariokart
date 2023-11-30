3c304a142a0feb9e525791e83b760ffd7005805d
Harry Stevenson <hjs115@student.bham.ac.uk>
2023-11-30 15:09:24 +0000

    Added function to process velocity value, removed '.' from whitlist

--- RL/Frame_Processing/Frame_Processor.py
+++ RL/Frame_Processing/Frame_Processor.py
@@ -1,45 +1,60 @@
 from PIL import Image
 import pytesseract as pt
-import cv2
+import re
 
-# Resolution: Native 640x528, bitrate:10,000, png compression: 6
-#imagePath = 'framedump_228.png'
-# Resolution: 2x Native - 720p, bitrate:10,000, png compression: 6
-imagePath = 'framedump_178.png'
-
-# Left, Upper, Right, Lower
-# Pixel coordinates of required values printed to screen by gecko code in native resolution
-#crop_regions = [(88, 177, 150, 191), (98,251,159,264), (149,269, 160,284)]
+path_to_framedumps = 'C:/Users/steve/OneDrive/Documents/Dolphin Emulator/Dump/Frames/framedump_'
 
 # Pixel coordinates of required values printed to screen by gecko code in 2xnative resolution
+# Left, Upper, Right, Lower
 crop_regions = [(176, 354, 248, 384), (185, 500, 286, 531), (290, 537, 312, 567)]
+
+completion_pattern = re.compile(r'^\d\.\d{5}$')
+mt_pattern = re.compile(r'^(?:\d{1,2}|1\d{2}|2[0-6]\d|27[0-0])$')
 
 # Splits screenshot into seperate images:
 # [0] = XZ Velocity
 # [1] = Race%
 # [2] = MT
 
-def crop_image(image):
+def crop_image(image: Image):
 # Inititalise empty array
     cropped_images = []
     for i,region in enumerate(crop_regions):
         cropped_images.append(image.crop(region))
-        cropped_images[i].save(f'{i}.png')
+        #cropped_images[i].save(f'{i}.png')
     return cropped_images
+    
+def format_velocity(vel:str):
+    # from 0.00 to 999.99
+    # if there is no decimal point, convert it to a float and divide by 100
+    if vel.isnumeric():
+        vel = float(vel)/100
+    return vel
 
-# Open image
-frame = Image.open(imagePath)
-# Convert image to black and white
-# Loop to check any BW threshold is effective
+print(format_velocity('3740'))
+def format_completion(completion:str):
+    return 1
+def format_mt(mt:str):
+    return 1
 
-frame = frame.convert('L')
-# Crop into sections
-cropped_images = crop_image(frame)
-# Extract text
-#print('pyTesseract')
-#print(pt.image_to_string(frame, config='-c tessedit_char_whitelist=0123456789.'))
+def process_frame(frame_index: int):
+    # Append index to file name
+    imagePath = path_to_framedumps + frame_index
+    print(f'read frame {frame_index}')
+    # Initialise empty array for storing race values
+    raceInfo = []
+    # Open image
+    frame = Image.open(imagePath)
 
-for i in range(3):
-    print(i)
-    text = pt.image_to_string(cropped_images[i], config='--psm 6 -c tessedit_char_whitelist=0123456789.')
-    print(text)
+    # Convert image to black and white
+    frame = frame.convert('L')
+
+    # Crop into sections
+    cropped_images = crop_image(frame)
+
+    # Extract text from images
+    for i in range(3):
+        text = pt.image_to_string(cropped_images[i], config='--psm 6 -c tessedit_char_whitelist=0123456789')
+        raceInfo[i] = text
+    
+    return raceInfo
