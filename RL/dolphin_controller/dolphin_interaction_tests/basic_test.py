927cfdc0100f62df4aab98e7209c23c16d02ffcc
hjs115 <hjs115@student.bham.ac.uk>
2023-12-18 13:35:09 +0000

    Dolphin Controller tests now interact successfully with dolphin

--- RL/dolphin_controller/dolphin_interaction_tests/basic_test.py
+++ RL/dolphin_controller/dolphin_interaction_tests/basic_test.py
@@ -1,14 +1,5 @@
 from dolphin import event, gui
 
 red = 0xffff0000
-frame_counter = 0
 
-# Call this function on frame advance (updates every frame)
-@event.on_frameadvance()
-def update():
-    frame_counter += 1
-    # Draw on screen
-    gui.draw_text((10, 10), red, f"Frame: {frame_counter}")
-    # Print to console
-    if frame_counter % 60 == 0:
-        print(f"The frame count has reached {frame_counter}")
+gui.draw_text((10, 10), red, "Hello from VSCode")
--- RL/dolphin_controller/dolphin_interaction_tests/spam_a.py
+++
@@ -1,6 +0,0 @@
-from dolphin import controller
-from time import sleep
-while True:
-    controller.set_gc_buttons(0, {"A": True})
-    sleep(0.05)
-    controller.set_gc_buttons(0, {"A": False})
