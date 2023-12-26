459b4bc8e647d6f6e7152487181eee2b2dda2890
hjs115 <hjs115@student.bham.ac.uk>
2023-12-26 17:44:07 +0000

    Added naiive savestate loading

--- RL/environment/env.py
+++ RL/environment/env.py
@@ -1,5 +1,8 @@
-#from dolphin import savestate # for loading savestates
-#from dolphin import event # for resetting emulation
+from dolphin import savestate # for loading savestates
+# from dolphin import event # for resetting emulation
+from dolphin import gui # for status messages
+from pyautogui import press
+
 from os import system  # for running dolphin from command line
 
 #TODO : Insert wrapper script here
@@ -9,20 +12,36 @@ CONFIG_PATH = 'Z:/project/hjs115/Dolphin/Config'
 # path to ISO game file
 ISO_PATH = 'Z:/ISO/MarioKartWii.rvz'
 # Command line options for Dolphin
-DOLPHIN_CONFIG = f'-e {ISO_PATH} -u {CONFIG_PATH} ' #--script {SCRIPT_PATH}'
+DOLPHIN_CONFIG = f'-e {ISO_PATH} -u {CONFIG_PATH}' #--script {SCRIPT_PATH}'
 # path to dolphin executable - use no GUI
-DOLPHIN_PATH = 'Z:/project/hjs115/Dolphin/Build/DolphinNoGUI.exe'
+DOLPHIN_PATH = 'Z:/project/hjs115/Dolphin/Build/Dolphin.exe'
+
+    # Default savestate will be in slot 1,
+    # Current issue with dolphin causing a deadlock when loading savestetes from scripts
+    # https://github.com/TASLabz/dolphin/issues/123
+    # savestate.load_from_slot(1)
+    # Instead we use the load state hotkey
+    # TODO: Change config so 'load state' button is on the gamecube controller and use
+    # controller.set_gc_buttons() to load the state
+    
+
 
 def init():
-    # Open MKWii in Dolphin
-    command = f'{DOLPHIN_PATH} {DOLPHIN_CONFIG}'
-    print(command)
-    system(command)
-    
-    # Load dolphin configs
-    # Load Savestate
-    # Pause Emulation
-    return 
+
+    # 1.) Open MKWii in Dolphin
+    # TODO: Dolphin resolution configs
+    #command = f'{DOLPHIN_PATH} {DOLPHIN_CONFIG}'
+    #print(command)
+    #system(command)
+
+    # 2.)Load dolphin configs
+    # 3.)Load Savestate
+    gui.add_osd_message("Loading state from slot 1")
+    press('f1')
+    # 4.) Pause Emulation
+    return
+
+
 
 def step():
     # Read Frame
@@ -38,4 +57,5 @@ def reset():
     # Reset Controller
     # wipe framedumps folder
     return
+
 init()
