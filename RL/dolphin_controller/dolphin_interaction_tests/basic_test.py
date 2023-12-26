96c4ad957dc49a786b996df408498f6c1597bb41
hjs115 <hjs115@student.bham.ac.uk>
2023-12-26 17:46:13 +0000

    Various dolphin data files - will be removed later

--- RL/dolphin_controller/dolphin_interaction_tests/basic_test.py
+++
@@ -1,5 +0,0 @@
-from dolphin import event, gui
-
-red = 0xffff0000
-
-gui.draw_text((10, 10), red, "Hello from VSCode")
--- RL/dolphin_controller/dolphin_interaction_tests/press_buttons.py
+++
@@ -1,8 +0,0 @@
-from dolphin import controller, gui
-from time import sleep
-red = 0xffff0000
-
-gui.add_osd_message("Pressing A", 2000,red)
-
-controller.set_gc_buttons(0, {"A": True})
-controller.get_gc_buttons(0)
