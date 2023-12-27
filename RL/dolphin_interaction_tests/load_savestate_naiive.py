98b58e4c7f9f56d6ddfa6218265d1fd69fcc927e
hjs115 <hjs115@student.bham.ac.uk>
2023-12-27 13:49:17 +0000

    Added both ways of loading savestate

--- RL/dolphin_interaction_tests/load_savestate_naiive.py
+++ RL/dolphin_interaction_tests/load_savestate_naiive.py
@@ -1,8 +1,19 @@
-from dolphin import gui
+from dolphin import gui, savestate
+from time import sleep
 from pyautogui import press
-
 red = 0xffff0000
-message = "Pressing f to load savestate in slot 1"
-gui.add_osd_message(message, 2000,red)
-print(message)
-press('f1')
+def load_using_api():
+    message = "loading savestate using dolphin api"
+    gui.add_osd_message(message, 2000,red)
+    # sleep for 1 second to avoid deadlock
+    sleep(1)
+    savestate.load_from_slot(1)
+
+def load_using_fkey():
+    message = " loading savestate using fkey"
+    gui.add_osd_message(message, 2000,red)
+    # sleep for 1 second to avoid deadlock
+    sleep(1)
+    press('f1')
+
+load_using_fkey()
