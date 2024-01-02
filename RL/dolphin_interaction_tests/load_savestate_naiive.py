8b3a80dee069baac1ca94e6fb3e01611d16889c7
hjs115 <hjs115@student.bham.ac.uk>
2024-01-02 17:38:36 +0000

    Improved comments

--- RL/dolphin_interaction_tests/load_savestate_naiive.py
+++ RL/dolphin_interaction_tests/load_savestate_naiive.py
@@ -21,16 +21,17 @@ def load_using_api():
     sleep(1)
     savestate.load_from_slot(1)
 
+# Loads the savestate in the first slot by pressing the default hotkey (F1)
 def load_using_fkey():
     
-    message = "loading savestate using fkey"
+    # status message
+    message = "loading savestate using f-key"
     gui.add_osd_message(message, 2000,red)
 
     # Using pynput to press hotkey for loading savestate in first slot
     virtual_keyboard = Controller()
-    
     virtual_keyboard.press(Key.f1)
-    sleep(0.1)
     virtual_keyboard.release(Key.f1)
 
+#register listener
 event.on_frameadvance(load_using_fkey)
