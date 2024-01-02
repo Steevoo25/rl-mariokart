202c871856abec5e124175fee80a55053d237782
hjs115 <hjs115@student.bham.ac.uk>
2024-01-02 13:43:37 +0000

    Added appending local python path, allowing use of packages within scripting dolphin build

--- RL/dolphin_interaction_tests/load_savestate_naiive.py
+++ RL/dolphin_interaction_tests/load_savestate_naiive.py
@@ -1,7 +1,15 @@
 from dolphin import gui, savestate
 from time import sleep
+
+# As the script is run within the dolphin executable, 
+# Append the true path of scripts to import
+from sys import path
+path.append("C:\\Users\\Harry Stevenson\\AppData\\Local\\Programs\\Python\\Python311")
+# now import required package
 from pyautogui import press
+# used for osd msg
 red = 0xffff0000
+
 def load_using_api():
     message = "loading savestate using dolphin api"
     gui.add_osd_message(message, 2000,red)
@@ -14,6 +22,6 @@ def load_using_fkey():
     gui.add_osd_message(message, 2000,red)
     # sleep for 1 second to avoid deadlock
     sleep(1)
-    press('f1')
+    #press('f1')
 
 load_using_fkey()
