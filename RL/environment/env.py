2571c2c4680ffec4486bcc8e581e429967afb3d6
hjs115 <hjs115@student.bham.ac.uk>
2024-01-02 17:52:58 +0000

    Same as last commit

--- RL/environment/env.py
+++ RL/environment/env.py
@@ -4,30 +4,24 @@
 #from dolphin import gui # for status messages
 
 # -- OTHER IMPORTS --
-from pyautogui import press
-from os import system  # for running dolphin from command line
+from sys import path
 
-#TODO : Insert wrapper script here
-SCRIPT_PATH = ''
-# path to dolphin config files
-CONFIG_PATH = 'Z:\\project\\hjs115\\Dolphin\\Config'
-# path to ISO game file
-ISO_PATH = 'Z:\\ISO\\MarioKartWii.rvz'
-# Command line options for Dolphin
-DOLPHIN_CONFIG = f' -e {ISO_PATH} -u {CONFIG_PATH}' #--script {SCRIPT_PATH}'
-# path to dolphin executable - use no GUI
-DOLPHIN_PATH = 'Z:\\Users\\Harry Stevenson\\Documents\\project\\dolphin-scripting\\repo\\dolphin\\Binary\\x64\\Dolphin.exe'
 
-    # Default savestate will be in slot 1,
-    # Current issue with dolphin causing a deadlock when loading savestetes from scripts
-    # https:\\github.com/TASLabz/dolphin/issues/123
+# Current issue with dolphin causing a deadlock when loading savestetes from scripts
+# https:\\github.com/TASLabz/dolphin/issues/123
 
 def init():
-# Running dolphin in command line causes some issues, so I will have to open dolphin and run the script through the gui OR use the 
-    command = DOLPHIN_PATH + DOLPHIN_CONFIG
-    print(command)
-    # 2.)Load dolphin configs
-    # 3.)Load Savestate
+# Running dolphin in command line causes some issues, 
+# so I will have to open dolphin and run the script through the gui,
+# making sure to use the correct config
+    
+    venv_dir ='Z:\\project\\hjs115\\venv\\Lib\\site-packages'
+    path.append(venv_dir)
+    # Adds my venv directory to the path of pytohn used by dolphin
+    path.append('Z:\\project\\hjs115')
+
+    # 3.) Load Savestate
+
     # 4.) Pause Emulation
     return
 
