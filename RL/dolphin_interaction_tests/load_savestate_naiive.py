a13118e242b51e91cc308a41bd7fd3889dae74d0
hjs115 <hjs115@student.bham.ac.uk>
2024-01-02 15:24:30 +0000

    Savestate loading now works, but due to callback on frameadvance, it loops infinitely

--- RL/dolphin_interaction_tests/load_savestate_naiive.py
+++ RL/dolphin_interaction_tests/load_savestate_naiive.py
@@ -1,18 +1,17 @@
-from dolphin import gui, savestate
+from dolphin import gui, savestate, event
 from time import sleep
 
 # As the script is run within the dolphin executable, 
 # Append the true path of scripts to import
-
 from sys import path
-
 venv_dir ='Z:\\project\\hjs115\\venv\\Lib\\site-packages'
 path.append(venv_dir)
-for dir in path:
-    gui.add_osd_message(dir)
+path.append('Z:\\project\\hjs115\\RL\\dolphin_interaction_tests')
+
 # now import required package
-from pyautogui import press
-# used for osd msg
+from pynput.keyboard import Controller, Key
+
+# used for osd msgs
 red = 0xffff0000
 
 def load_using_api():
@@ -23,10 +22,15 @@ def load_using_api():
     savestate.load_from_slot(1)
 
 def load_using_fkey():
-    message = " loading savestate using fkey"
+    
+    message = "loading savestate using fkey"
     gui.add_osd_message(message, 2000,red)
-    # sleep for 1 second to avoid deadlock
-    sleep(1)
-    press('f1')
 
-load_using_fkey()
+    # Using pynput to press hotkey for loading savestate in first slot
+    virtual_keyboard = Controller()
+    
+    virtual_keyboard.press(Key.f1)
+    sleep(0.1)
+    virtual_keyboard.release(Key.f1)
+
+event.on_frameadvance(load_using_fkey)
