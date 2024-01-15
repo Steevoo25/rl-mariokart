1865b3cffc5813b7a6450129c353ffbef9b7cb57
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-01-15 13:17:22 +0000

    CHanged status message to print to log instead of on-screen

--- RL/dolphin_interaction_tests/press_buttons.py
+++ RL/dolphin_interaction_tests/press_buttons.py
@@ -2,7 +2,7 @@ from dolphin import controller, gui
 from time import sleep
 red = 0xffff0000
 
-gui.add_osd_message("Pressing A", 2000,red)
+print("Pressing A")
 
 controller.set_gc_buttons(0, {"A": True})
 controller.get_gc_buttons(0)
