b306b11c02db3bccfdb1fda55863ba898d4a841d
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-01-16 15:14:00 +0000

    Updated steps

--- RL/dolphin_interaction_tests/load_savesatete_very_naiive.py
+++ RL/dolphin_interaction_tests/load_savesatete_very_naiive.py
@@ -1,1 +1,13 @@
 # Navigate through menus and get to start of training
+# Inputs needed
+#  Wiimote safety Screen - A
+# MKWii main screen - A
+# License selection - A
+# Number of players selection - A
+# Gamemode selection - Down, A
+# Character - right right, down down down A
+# kart selection - right down a
+# drift type - a
+# track selection - right right right down a a
+# time trial type selection - down down a
+# confirmation a
