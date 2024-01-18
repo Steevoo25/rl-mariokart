c5ee70ad3d9a0b431043dae92b74233192905d19
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-01-18 14:21:15 +0000

    Changed back to loop through dump folder

--- RL/Frame_Processing/Frame_Processing_Test.py
+++ RL/Frame_Processing/Frame_Processing_Test.py
@@ -2,6 +2,5 @@ from Frame_Processor import process_frame
 # A short script that gets frame data from every framedump in the dolphin framedumps folder
 movieInfo = []
 
-# for i in range(90, 200):
-#        print(f'Frame: {i}\nData:{process_frame(i)}')#
-print(process_frame(90))
+for i in range(90, 200):
+        print(f'Frame: {i}\nData:{process_frame(i)}')#
