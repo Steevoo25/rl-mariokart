9cb991b66dd19f496005d8cb6a521d25fcb0ecf1
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-01-26 13:24:39 +0000

    Renamed memory script, Added Speed, Added wrapper function

---
+++ RL/environment/Memory/memory_viewer.py
@@ -0,0 +1,30 @@
+from dolphin import memory, event
+
+# Memory locations of required values, found using Dolphin Memory Engine
+# https://github.com/aldelaro5/Dolphin-memory-engine
+RACE_COMPLETION_LOCATION = 0x80E43708
+MT_LOCATION = 0x80E4C756
+SPEED_LOCATION = 0x80E4C678
+
+# Returns the current speed of the kart
+def getCurrentSpeed() -> float:
+    return memory.read_f32(SPEED_LOCATION)
+
+# Returns the value of the players current race completion (Race%)
+def getCurrentRaceCompletion() -> float:
+    return memory.read_f32(RACE_COMPLETION_LOCATION)
+
+# Returns the value of the player's current miniturbo charge (MT)
+def getCurrentMT() -> int:
+    return memory.read_u16(MT_LOCATION)
+    
+# Returns a tuple of the Speed, Race% and MT
+def getRaceInfo() -> (float, float, int):
+    return getCurrentSpeed(), getCurrentRaceCompletion(), getCurrentMT()
+
+def printRaceInfo():
+    print(f'MT: {getCurrentMT()}')
+    print(f'Race%: {getCurrentRaceCompletion()}')
+    print(f'Speed: {getCurrentSpeed()}')
+
+#event.on_frameadvance(printRaceInfo)
