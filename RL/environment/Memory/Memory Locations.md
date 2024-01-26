ec64e91cc942f8c29d018c2d6d36d11a07b35bda
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-01-26 12:34:49 +0000

    Added race%

--- RL/environment/Memory/Memory Locations.md
+++ RL/environment/Memory/Memory Locations.md
@@ -5,7 +5,9 @@ Source: https://docs.google.com/spreadsheets/d/1gA5WmnEbPAeA1Lq4XUJg9qDwawky9hpN
 |---|---|---|---|
 |0x809b8f70|0xF8 | ptr_raceInfo | Current Race Completion|
 
+
 Found using DME - https://github.com/aldelaro5/Dolphin-memory-engine
 
 Race% = 0x80E43708
+Type = float (f32)
 
--- RL/environment/Memory/Pointers.py
+++ RL/environment/Memory/Pointers.py
@@ -2,6 +2,8 @@ from dolphin import memory
 # This script contains modified versions of functions from Ben Middleton's MKW AI Environment
 # https://github.com/benjaminjmiddleton/mkw_ai_env
 
+RACE_COMPLETION_LOCATION = 0x80E43708
+
 def getGameID():
     # 6-byte string at address 0x80000000
     # For the version I am using this will always be 'RMCE01'
@@ -19,15 +21,14 @@ def getPointerChain(pointer, offsets):
             break
     return pointer
 
+# returns the pointer value of the current checkpoint the player is in
 def getCurrentCheckpointPointer(Offset):
     pointer = 0x80000000
     pointer += 0x9B8F70
     return getPointerChain(pointer, [0xC, Offset, 0x24])
 
-def getCurrentRaceCompletionPointer(Offset):
-    pointer = 0x80000000
-    pointer += 0x809b8f70
-    return getPointerChain(pointer, [Offset])
+def getCurrentCheckpoint() -> float:
+    return memory.read_u16(getCurrentCheckpointPointer(0x0))
 
-
-print(memory.read_u16(getCurrentCheckpointPointer(0x0)))
+def getCurrentRaceCompletion() -> float:
+    return memory.read_f32(RACE_COMPLETION_LOCATION)
