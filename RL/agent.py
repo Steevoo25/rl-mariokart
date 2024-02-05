85391002d413e4694df8c602211b9ff6223881b7
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-05 11:44:53 +0000

    start of agent script

---
+++ RL/agent.py
@@ -0,0 +1,9 @@
+from dolphin import event
+
+from sys import path
+# Add venv dir to path to allow external packages
+venv_dir ='C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\venv\\Lib\\site-packages'
+path.append(venv_dir)
+# Add this dir to path to allow imports from other scripts
+this_dir = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\RL\\dolphin_interaction_tests'
+path.append(this_dir)
