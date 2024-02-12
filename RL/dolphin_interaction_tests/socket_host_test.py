19c914c56fc35df4eb0035c39acd76d9476a3202
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-12 15:44:58 +0000

    update documentation

--- RL/dolphin_interaction_tests/socket_host_test.py
+++ RL/dolphin_interaction_tests/socket_host_test.py
@@ -1,13 +1,18 @@
 import socket
 
+# A test script to connect to dolphin and accept a message (the current value of frame counter)
+# -- set up Host socket
 HOST = socket.gethostname()
 PORT = 5555
 sock = socket.socket()
 sock.bind((HOST, PORT))
+# -- wait for a connection
 sock.listen(1)
+# -- accept connection
 connection, address = sock.accept()
+# -- print connection address
 print("Connected by ", address)
-
-#recieve 4 byte integer
-message = connection.recv(1024)
+#recieve bytes
+message = connection.recv(4)
+# print recieved value
 print(int.from_bytes(message, byteorder="big"))
