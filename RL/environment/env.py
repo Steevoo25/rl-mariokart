220842533e47d12da3c0dea3cd95cee4365beeda
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-12 14:29:19 +0000

    Setup socket config and frame counter

--- RL/environment/env.py
+++ RL/environment/env.py
@@ -17,6 +17,8 @@ this_dir = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project
 path.append(this_dir)
 
 # -- OTHER IMPORTS --
+import socket
+
 from load_savestate import load_using_fkey as load_savestate
 from press_button import press_button as set_controller
 from calculate_reward import calculate_reward
@@ -31,7 +33,7 @@ def reset():
 # making sure to use the correct config
     # 1.) Load Savestate
     # Because init() will be called in a frameadvance, I dont need it now
-    total_reward = 0
+    
     load_savestate()
     # 2.) Reset Controller
     set_controller(DEFAULT_CONTROLLER)
@@ -51,13 +53,26 @@ def step():
     # identify input with highest estimated reward
     return reward
 
+
+## Socket Initialisation
+# This script will be running within Dolphin's embedded python, meaning it has a lot of limitations
+# To get around these I will send the environment data to a seperate Agent script
+
+HOST = socket.gethostname()
+PORT = 5555
+print(f'Host : {HOST}')
+env_socket = socket.socket()
+#env_socket.connect((HOST,PORT)) # Uncomment when training
+
+## Initialisations
+previous_frame_info = 0
+reward = 0
 frame_counter = 0
 
 ### Main Training Loop ###
 just_reset = False
 
 while True:
-    event_data = await event.frameadvance()
+    await event.frameadvance()
     frame_counter +=1
     print(f"Frame: {frame_counter}")
-    print(f"Event await data: {event_data}")
