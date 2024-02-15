38cb7efc0bebec1be24d26f896cbdfee5c1c4d00
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-15 12:28:15 +0000

    Added: Gets response from rainbow, logging mode, reward setting on resetting episode

--- RL/environment/dolphin_env.py
+++ RL/environment/dolphin_env.py
@@ -51,33 +51,40 @@ termination_flag = False
 action = DEFAULT_CONTROLLER
 frameInfo_previous = [0,0,0]
 reward_set = False
+logging = True
+reset_requested = False
+
 
 ### Main Training Loop ###
 
 while True:
     await event.frameadvance()
     frame_counter +=1
+    
     # Get frameInfo
     if frame_counter == 1:
+    # If episode has jsut started don't reset
         frameInfo_current = [0,0,0]
         termination_flag = False
         total_reward = 0
     else:
         frameInfo_current = getRaceInfo()
-        
-    # Get response from previous frame
-    # -----
-    # action
-    
-        # recieve action from Rainbow
-        action = DEFAULT_CONTROLLER
-    # Check termination
-    # -----
-        accelerating = frameInfo_current[0] > frameInfo_previous[0]
-        termination_flag = check_termination(frameInfo_current, accelerating)
+        # --- Get response from Rainbow based on previous frame
+        response = json.loads(env_socket.recv(131702).decode("utf-8"))
+        action = response[0]
+        reset_requested = response[1]
+        # --- Check termination
+        if not reset_requested: 
+        # If rainbow hasnt requested reset then check my own conditions
+            accelerating = frameInfo_current[0] > frameInfo_previous[0]
+            termination_flag = check_termination(frameInfo_current, accelerating)
+        else:
+        # If rainbow has requested reset then reset
+            termination_flag = True
 
         if termination_flag:
-            reward = -10
+        # Reset
+            total_reward = -1000 # Tweak number
             reward_set = True
             frame_counter = 0
             just_reset = True
@@ -89,21 +96,15 @@ while True:
     # -- Calculate reward
     if not reward_set:
         reward = calculate_reward(frameInfo_current, frameInfo_previous)
-        reward_set = True
-    # update previous_frame_info 
-    frameInfo_previous = getRaceInfo()
-    # update total reward
-    total_reward = total_reward + reward
-    # Rainbow is configured to take 4 things
-    # pixel Value - this is done within the rainbow process
-        # Print state to dolphin log
-    print_state_to_dolphin_log(frame_counter, *frameInfo_current, termination_flag, reward, total_reward)
-    # Reward Value
-    # termination flag
-    # frame counter
+        # update previous_frame_info 
+        frameInfo_previous = frameInfo_current
+        # update total reward
+        total_reward = total_reward + reward
+    if logging:
+    # Print state to dolphin log
+        print_state_to_dolphin_log(frame_counter, *frameInfo_current, termination_flag, reward, total_reward)
     # -- Send data to Rainbow
     # encode data as json object and send to agent process
-
     data_to_send = json.dumps((reward, termination_flag, frame_counter)).encode("utf-8")
     
     # env_socket.sendall(data_to_send)
