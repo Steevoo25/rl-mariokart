7eca1f1516018243f009d219b938b87b89303f84
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-20 13:36:38 +0000

    Added functionality for choosing random action

--- RL/q-learning-agent.py
+++ RL/q-learning-agent.py
@@ -3,29 +3,32 @@ import random
 # Constants
 eps = 0.2
 
+START_STATE = (1.128, 0.992, 0)
+
 # Action Space
     # Action tuples contains a list of all possible inputs as tuples
 from Rainbow.AdditionalScripts.action_space import generate_action_space
 action_space = generate_action_space()
+TOTAL_ACTIONS = len(action_space)
+
+# Convert array of dictionaries to array of tuples
 action_tuples = []
 for dicts in action_space:
     action_tuples.append(tuple(dicts.values()))
+    
 # State Space
-
+sample_state = (24.5, 34.5, 3)
 # Q-Table
     # Table of Q values for each state-action pair
-    # q[(s,a)] = q[(("vel": float, "RacePerc": float, "mt": int), )]
-# I dont want to store all the q-values for every state and action straight away
-# I will check if a value exists and return 0 if not
+    # q[(s,a)] = q[((float, float, int), (Bool, Bool, Bool, int))]
 q = {}
-# q[()]
 # Epsilon-Greedy Policy
 def epsilon_greedy(state, eps):
     # Pick random action with probability epsilon
     if random.uniform(0,1) < eps:
-        return 1
+        return action_tuples[random.randint(0,TOTAL_ACTIONS - 1)]
     # Choose best action in current state with probability epsilon
     else:
-        return 0
-        
+        return max()
+print(epsilon_greedy(1, eps))
 # Update Rule
