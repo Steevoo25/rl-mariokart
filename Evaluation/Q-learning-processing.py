ce481a5cbae1a4689b4d976607d0c65ab5092e2b
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-27 17:19:46 +0000

    Added plot for Return and Survival Time

--- Evaluation/Q-learning-processing.py
+++ Evaluation/Q-learning-processing.py
@@ -1,7 +1,21 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
-
-path = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\q-learning-16-46--27_02_2024.csv'
-
+path = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\data\\q-learning-17-15--27_02_2024.csv'
+# Read into dataframe
 df = pd.read_csv(path)
-print(df)
+# Remove 1st episode (idk why -100 reward)
+df = df[1:]
+
+# Plotting
+plt.plot(df['Episode'], df['Total_Reward'], label='Return')
+plt.plot(df['Episode'], df['Frame_Count'], label='Survival Time')
+
+plt.xlabel('Episode number')
+plt.ylabel('Return/Survival Time')
+plt.title('Line Graph')
+
+plt.legend()
+
+plt.show()
+
