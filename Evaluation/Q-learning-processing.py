df6ba7a32ea7246c831ba569fa4a3b3b31c06cb9
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-27 16:51:16 +0000

    Added script for log data processing

---
+++ Evaluation/Q-learning-processing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+path = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\q-learning-16-46--27_02_2024.csv'
+
+df = pd.read_csv(path)
+print(df)
