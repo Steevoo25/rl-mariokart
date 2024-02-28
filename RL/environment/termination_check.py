dde56c8ccf25df4b6f7cba6883fe509321b3c6db
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-28 17:31:15 +0000

    Update for new savestate and cleanup

--- RL/environment/termination_check.py
+++ RL/environment/termination_check.py
@@ -1,18 +1,14 @@
 # A script that checks if the episode needs to terminate, given the current and previous raceInfo
 # Constants
-MIN_ACCEPTABLE_SPEED = 60
-END_OF_FIRST_STRAIGHT = 1.073
+MIN_ACCEPTABLE_SPEED = 70
 LAP_COMPLETED = 2
 RACE_COMPLETED = 4
 
-def check_termination(raceInfo, accelerating : bool):
+def check_termination(raceInfo) -> bool:
     vel, racePercent, mt = raceInfo
     
     # speed is too low
     if vel < MIN_ACCEPTABLE_SPEED:
-        # give some leeway at start of race -> dont terminate
-        if racePercent < END_OF_FIRST_STRAIGHT and accelerating:
-            return False
         return True
 
     if racePercent > LAP_COMPLETED:
