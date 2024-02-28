8170e54536228fd02afc7adbb1296488a43adb35
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-28 08:18:18 +0000

    documentation

--- Evaluation/replay_episode.py
+++ Evaluation/replay_episode.py
@@ -8,11 +8,12 @@ PROJECT_DIR = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-proj
 date_and_time = '22-08--27_02_2024'
 
 best_inputs_file = f"{PROJECT_DIR}Evaluation\\controller_episodes\\q-learning-{date_and_time}.pkl"
-
+# Load pickle file
 controller_inputs = pickle.load(open(best_inputs_file, "rb"))
-
+# Loop through file
 for i in range(len(controller_inputs)):
-    await event.frameadvance()
-    controller.set_gc_buttons(0,controller_inputs[i])
+    await event.frameadvance() # wait for dolphin
+    controller.set_gc_buttons(0,controller_inputs[i]) # set controller
 
+##TODO: Pause emulation
 gui.add_osd_message('End of episode')
