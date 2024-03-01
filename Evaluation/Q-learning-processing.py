99111a7fa7298503b43a5f7b25a18bced437761f
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-01 23:06:01 +0000

    Added saving png to file

--- Evaluation/Q-learning-processing.py
+++ Evaluation/Q-learning-processing.py
@@ -1,20 +1,20 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-
-path = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\q-learning-22-08--27_02_2024.csv'
+filename = 'q-learning-23-26--29_02_2024'
+path = f'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\data\\{filename}.csv'
 # Read into dataframe
 df = pd.read_csv(path)
-# Remove 1st episode (idk why -100 reward)
+# Remove episodes 1 and 3 (idk why -100 reward)
 df = df[1:]
 
 # Smooth the data
-smoothing_window = len(df) // 100
+smoothing_window = len(df) // 10
 
 smoothed_reward = pd.Series(df['Total_Reward']).rolling(window=smoothing_window, min_periods=1, center=True).mean()
 
 # Plotting
 #plt.plot(df['Episode'], df['Total_Reward'], label='Return', linewidth=0.1)
-plt.plot(df['Episode'], smoothed_reward, label='Return', linewidth=0.3)
+plt.plot(df['Episode'], smoothed_reward, label='Return', linewidth=1)
 #plt.plot(df['Episode'], df['Frame_Count'], label='Survival Time')
 
 # Set x axis scale
@@ -26,5 +26,11 @@ plt.ylabel('Return')
 plt.title(f'Return from each episode after {len(df)} Episodes')
 
 plt.legend()
+
+# Edit filepath to save png
+path = path.replace("csv", "png")
+path = path.replace("data", "plots")
+
+plt.savefig(path)
 
 plt.show()
