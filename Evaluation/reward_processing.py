b91548bce906d7cc89cbc66dc93d5cde63da82e4
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-05 16:29:31 +0000

    Added demo filepath constant

--- Evaluation/reward_processing.py
+++ Evaluation/reward_processing.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
+DEMO_FILE = 'q-learning-05_03_2024--16-00.csv'
 filepath = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\data\\q-learning\\rewards\\'
-filename = 'q-learning-05_03_2024--13-23.csv'
+filename = 'q-learning-05_03_2024--16-00.csv'
 
 rewards = pd.read_csv(f'{filepath}{filename}')
-column_names = ["total_reward","vel_reward","perc_reward","mt_reward"]
+column_names = ["total_reward","vel_reward","perc_reward","mt_reward","cp_reward"]
 print(rewards)
 
 x_axis = rewards.index.tolist()
