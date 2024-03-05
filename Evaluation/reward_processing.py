db9f72e35a032fa91538c9ed6ed045c6d72dbf4a
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-05 14:17:42 +0000

    filename for processing

--- Evaluation/reward_processing.py
+++ Evaluation/reward_processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 filepath = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\data\\q-learning\\rewards\\'
-filename = 'q-learning-05_03_2024--13-04.csv'
+filename = 'q-learning-05_03_2024--13-23.csv'
 
 rewards = pd.read_csv(f'{filepath}{filename}')
 column_names = ["total_reward","vel_reward","perc_reward","mt_reward"]
