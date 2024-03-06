c757e257076f78acd57c75c6ad9cb415a7eb7ac2
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-06 16:37:13 +0000

    Added time step for correct replaying

--- Evaluation/replay_episode.py
+++ Evaluation/replay_episode.py
@@ -3,17 +3,27 @@
 from dolphin import event, controller, gui
 import pickle
 
-
+TIME_STEP = 20
 PROJECT_DIR = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\'
-filename = 'q-learning-11-11--29_02_2024'
+filename = 'q-learning-06_03_2024--13-55'
 
-best_inputs_file = f"{PROJECT_DIR}Evaluation\\controller_episodes\\{filename}.pkl"
+# A helper function to convert a tuple of actions (used in the q-learning process) to a dictionary (to send to emulator)
+def convert_actions_to_dict(action: tuple):
+    return {"A": True, "B": action[0],"Up": action[1],"StickX": action[2]}
+
+best_inputs_file = f"{PROJECT_DIR}Evaluation\\data\\q-learning\\controller_episodes\\{filename}.pkl"
 # Load pickle file
 controller_inputs = pickle.load(open(best_inputs_file, "rb"))
 # Loop through file
-for i in range(len(controller_inputs)):
+counter = 0
+
+for i in range(len(controller_inputs) * TIME_STEP) :
     await event.frameadvance() # wait for dolphin
-    controller.set_gc_buttons(0,controller_inputs[i]) # set controller
+    if i % TIME_STEP == 0 : 
+        action = convert_actions_to_dict(controller_inputs[counter])
+        counter +=1
+    controller.set_gc_buttons(0,action) # set controller
+    
 
 ##TODO: Pause emulation
 gui.add_osd_message('End of episode')
