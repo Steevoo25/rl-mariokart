bbb1415806e5451b65a99a1c2d0ebf781e780aee
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-10 23:32:41 +0000

    update to check road type

--- RL/environment/termination_check.py
+++ RL/environment/termination_check.py
@@ -1,16 +1,18 @@
 # A script that checks if the episode needs to terminate, given the current and previous raceInfo
 # Constants
-MIN_ACCEPTABLE_SPEED = 69
+from memory_viewer import getCurrentRoadType
+MIN_ACCEPTABLE_SPEED = 60
 LAP_COMPLETED = 2
 RACE_COMPLETED = 4
 
 def check_termination(raceInfo) -> bool:
     vel, racePercent = raceInfo
     
+    if getCurrentRoadType() == 3:
+        return True
     # speed is too low
     if vel < MIN_ACCEPTABLE_SPEED:
         return True
-
     if racePercent > LAP_COMPLETED:
         return True
     # --- Uncomment if training for whole race
