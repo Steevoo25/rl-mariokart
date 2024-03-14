a0d68e1a4a9b58e53ce034262d018b2b5aea2f1a
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-14 07:48:16 +0000

    Turning in a wheelie is now no longer a valid action in one step (if one is started in one frame and the agent turns in the next it can still be performed)

--- RL/Rainbow/AdditionalScripts/action_space.py
+++ RL/Rainbow/AdditionalScripts/action_space.py
@@ -15,7 +15,9 @@ def generate_action_space():
     for button_state in button_states:
         for analog_state in analog_stick_angles:
             permutation_dict = {"B": button_state[0], "Up": button_state[1], "StickX": analog_state}
-            if not (button_state[0] == True and analog_state == 128):
+            jump_in_place = (button_state[0] == True and analog_state == 128)
+            turn_in_wheelie = (button_state[1] == True and not analog_state == 128)
+            if not (jump_in_place or turn_in_wheelie):
                 permutations.append(permutation_dict)
     return permutations
     
