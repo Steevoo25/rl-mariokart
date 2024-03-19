2087b13d351c0faa40f0bedc4c4a973439e7559a
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-19 15:19:36 +0000

    added 1-alpha in update rule

--- RL/q_learning_agent.py
+++ RL/q_learning_agent.py
@@ -41,7 +41,7 @@ def handle_unassigned_q_action(next_state, action):
 # --- Epsilon-Greedy Policy
 def epsilon_greedy(state, eps):
 
-    # If all actions have been explored, always move to it
+    # If all actions have been explored, always move to it]
     # count all occurences of state in q
     explored_actions = sum(1 for key in q.keys() if key[0] == state)
     #filtered = [(key, value) for key, value in q.items() if key[0] == state]
@@ -50,22 +50,32 @@ def epsilon_greedy(state, eps):
         #sorted_q = sorted(filtered, key=lambda x: handle_unassigned_q_action(x[0][0], x[0][1]))
         #print("sorted q of current state: ", sorted_q)
     # # Make proportional to visits to state
-    # if explored_actions >= ACTION_COUNT +1: 
-    #     eps = eps ** 2 # high chance of choosing best
-    #     print("Fully explored:", state, " count " , explored_actions)
+    if explored_actions >= ACTION_COUNT +1: 
+         eps = eps ** 2 # high chance of choosing best
+         print("Fully explored:", state, " count " , explored_actions)
 
-        
+    best_q = 0
+    best_action = "None found"
+    for action in action_tuples:
+        value = handle_unassigned_q_action(state, action)
+        print("Checking", action, ", Value:", value )
+        if handle_unassigned_q_action(state, action) > best_q:
+            best_action = action
+            best_q = value
+    print("best action found: ", best_action)
+
+    best_action =  action_tuples[max(range(ACTION_COUNT + 1), key= lambda x : handle_unassigned_q_index(state, x))]     
         #print list of fully explored state
     # Pick random action with probability epsilon
     if random.uniform(0,1) < eps or explored_actions == 0:
         random_action = action_tuples[random.randint(0, ACTION_COUNT)]
-        return random_action, "Exploring"
+        return random_action, f"Exploring - RANDOM (Best:{best_action})"
     # Choose best action in current state with probability 1-epsilon
     else:
         # Checking here - is it giving best action from q-table
         # in the case of an empty q table for state, lambda will always return 0, instead, rturn random
-        best_action =  action_tuples[max(range(ACTION_COUNT + 1), key= lambda x : handle_unassigned_q_index(state, x))] 
-        return best_action, "Exploiting"
+        #best_action =  action_tuples[max(range(ACTION_COUNT + 1), key= lambda x : handle_unassigned_q_index(state, x))] 
+        return best_action, "Exploiting - BEST"
         # Explanation of this line:
         # max() function gives index of best action, based on its q-value
         # action_tuples[] returns the action from its index
@@ -87,7 +97,7 @@ def update_q_table(prev_state, action, reward, next_state, alpha, gamma, eps) ->
     except KeyError:
         q[prev_state, action] = 0
 
-    q[prev_state, action] += alpha * (reward + (gamma * future_q[1]) - q[prev_state, action])
+    q[prev_state, action] += (1-alpha) * q[prev_state, action] + alpha * (reward + (gamma * future_q[1]) - q[prev_state, action])
     q[prev_state, action] = round(q[prev_state, action], 4)
     return q[prev_state, action]
 
