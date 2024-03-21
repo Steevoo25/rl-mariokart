fd1cdfd1329bf213e5fe7fae65a72d611a3a6426
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-21 15:05:04 +0000

    cleanup statically defined actions

--- RL/Rainbow/AdditionalScripts/action_space.py
+++ RL/Rainbow/AdditionalScripts/action_space.py
@@ -1,24 +1,9 @@
-# A script to generate all permutations of the abstracted controller
-
-from itertools import product
-# Returns and indexed dictionary of all controller permutations
 def generate_action_space():
-    # # Define the possible states for the buttons and analog stick
-    # buttons = [True, False]
-    # analog_stick_angles = [0, 64, 128, 192, 255]
-
-    # # Generate all possible permutations using itertools.product
-    # button_states = list(product(buttons, repeat=2))  # All possible combinations of 2 buttons
-
-    # # Generate permutations and store them in a list of dictionaries
-    # permutations = []
-    # for button_state in button_states:
-    #     for analog_state in analog_stick_angles:
-    #         permutation_dict = {"B": button_state[0], "Up": button_state[1], "StickX": analog_state}
-    #         jump_in_place = (button_state[0] == True and analog_state == 128)
-    #         turn_in_wheelie = (button_state[1] == True and not analog_state == 128)
-    #         if not (jump_in_place or turn_in_wheelie):
-    #             permutations.append(permutation_dict)
-    permutations = [{"B": True, "Up": False, "StickX": 0},{"B": True, "Up": False, "StickX": 64},{"B": True, "Up": False, "StickX": 192},{"B": True, "Up": False, "StickX": 255},{"B": False, "Up": True, "StickX": 128},{"B": False, "Up": False, "StickX": 64},{"B": False, "Up": False, "StickX": 192}]
+    permutations = [{"B": True, "Up": False, "StickX": 0}, # hard drift left
+    {"B": True, "Up": False, "StickX": 64}, # soft drift left
+    {"B": True, "Up": False, "StickX": 192}, # soft drift right
+    {"B": True, "Up": False, "StickX": 255}, # hard drift right
+    {"B": False, "Up": True, "StickX": 128}, # wheelie straight
+    {"B": False, "Up": False, "StickX": 64}, # slight turn left
+    {"B": False, "Up": False, "StickX": 192}] # sligh turn right
     return permutations
-    
