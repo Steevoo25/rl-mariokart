128ae9a62ea64a92561e97defbdc429c68a9f139
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-22 17:19:32 +0000

    y-limits

--- Evaluation/reward_processing.py
+++ Evaluation/reward_processing.py
@@ -3,16 +3,19 @@ from matplotlib import pyplot as plt
 
 DEMO_FILE = 'q-learning-05_03_2024--16-00.csv'
 filepath = 'C:\\Users\\steve\\OneDrive\\Documents\\3rd Year\\Project\\my-project\\Evaluation\\data\\q-learning\\rewards\\'
-filename = 'q-learning-07_03_2024--18-25.csv'
+filename = 'q-learning-05_03_2024--13-23.csv'
 
 rewards = pd.read_csv(f'{filepath}{filename}')
-column_names = ["total_reward","vel_reward","perc_reward","mt_reward","cp_reward"]
-print(rewards)
+column_names = ["total_reward","vel_reward","perc_reward","mt_reward"]
+#print(rewards)
 
 x_axis = rewards.index.tolist()
 
 for column in column_names:
     plt.plot(x_axis,rewards[column] , label=column)
     
+# Set y axis scale
+plt.ylim(0, 4000)
+
 plt.legend()
 plt.show()
