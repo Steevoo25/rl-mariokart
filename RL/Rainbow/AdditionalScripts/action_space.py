8300c45dbb4b45b14ec84ab4e4412c2f6945b303
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-28 16:40:05 +0000

    add driving straight

--- RL/Rainbow/AdditionalScripts/action_space.py
+++ RL/Rainbow/AdditionalScripts/action_space.py
@@ -4,6 +4,7 @@ def generate_action_space():
     {"B": True, "Up": False, "StickX": 192}, # soft drift right
     {"B": True, "Up": False, "StickX": 255}, # hard drift right
     {"B": False, "Up": True, "StickX": 128}, # wheelie straight
+    {"B": False, "Up": False, "StickX": 128}, # Drive straight
     {"B": False, "Up": False, "StickX": 64}, # slight turn left
     {"B": False, "Up": False, "StickX": 192}] # sligh turn right
     return permutations
