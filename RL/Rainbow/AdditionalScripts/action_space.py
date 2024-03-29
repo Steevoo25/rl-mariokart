01abd73796d9e7bedeaeaa0e42d46feda20312ae
hjs115 <hjs115@student.bham.ac.uk>
2024-03-29 15:56:38 +0000

    refactor

--- RL/Rainbow/AdditionalScripts/action_space.py
+++ RL/Rainbow/AdditionalScripts/action_space.py
@@ -1,10 +1,10 @@
 def generate_action_space():
-    permutations = [{"B": True, "Up": False, "StickX": 0}, # hard drift left
+    actions = [{"B": True, "Up": False, "StickX": 0}, # hard drift left
     {"B": True, "Up": False, "StickX": 64}, # soft drift left
     {"B": True, "Up": False, "StickX": 192}, # soft drift right
     {"B": True, "Up": False, "StickX": 255}, # hard drift right
     {"B": False, "Up": True, "StickX": 128}, # wheelie straight
-    {"B": False, "Up": False, "StickX": 128}, # Drive straight
+    {"B": False, "Up": False, "StickX": 128}, # drive straight
     {"B": False, "Up": False, "StickX": 64}, # slight turn left
-    {"B": False, "Up": False, "StickX": 192}] # sligh turn right
-    return permutations
+    {"B": False, "Up": False, "StickX": 192}] # slight turn right
+    return actions
